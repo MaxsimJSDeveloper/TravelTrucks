b9324f67881d52a1658898661659bf1478db28f0
Holovko Maksym <golovkomaksim852@gmail.com>
2024-09-27 16:49:08 +0300

    Reviews

--- src/components/CamperReviews/CamperReviews.jsx
+++ src/components/CamperReviews/CamperReviews.jsx
@@ -1,4 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectCamper } from "../../redux/campers/selectors";
+import Review from "../Review/Review";
+
+import css from "./CamperReviews.module.css";
+
 const CamperReviews = () => {
-  return <h1>CamperReviews</h1>;
+  const { reviews = [] } = useSelector(selectCamper);
+
+  return (
+    <div className={css.container}>
+      <ul className={css.reviewList}>
+        {reviews.map((review, index) => (
+          <li key={index}>
+            <Review review={review} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default CamperReviews;
---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,45 @@
+import sprite from "../../img/symbol-defs.svg";
+import css from "./Review.module.css";
+
+const Review = ({ review }) => {
+  const { reviewer_name, reviewer_rating, comment } = review;
+
+  const initial = reviewer_name[0].toUpperCase();
+
+  const stars = Array.from(
+    { length: 5 },
+    (_, index) => index < reviewer_rating
+  );
+
+  return (
+    <div className={css.reviewContainer}>
+      <div className={css.user}>
+        <div className={css.avatar}>
+          <span>{initial}</span>
+        </div>
+        <div className={css.infoContainer}>
+          <p className={css.name}>{reviewer_name}</p>
+          <ul className={css.stars}>
+            {stars.map((filled, index) => (
+              <li key={index}>
+                <svg className={css.star}>
+                  <use
+                    xlinkHref={`${sprite}#${
+                      filled ? "icon-star-active" : "icon-star"
+                    }`}
+                  />
+                </svg>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <p className={css.comment}>{comment}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
